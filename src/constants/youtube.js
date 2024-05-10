aed2eec780ebc9ea58098b88cd75a63fba61815f
Mugunthan <mugunthansjc05@gmail.com>
2024-05-10 12:28:36 +0530

    updated new files

---
+++ src/constants/youtube.js
@@ -0,0 +1,5 @@
+export const API_KEY = 'AIzaSyA7T_QhbgfyyzuSe__pdVe1s6ESFNcU47Y';
+export const BASE_URL = 'https://www.googleapis.com/youtube/v3';
+export const YOUTUBE_VIDEO = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`;
+
+export default API_KEY
